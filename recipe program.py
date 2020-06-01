9b66b1c736bedee031e7c18b4c63e3bfdd4a45f1
Olivia Goodman <oliviagoodman@Olivias-Air-2.hub>
2020-06-01 16:57:47 +1200

    Version One
    In this version I set up the program with a pre-made recipe so I could focus on getting the scale factor to work. The user inputs how many people the recipe should serve, then the program divides this by how many people it originally served to find the scale factor. This is what all the values in the amounts list will be multiplied by.

--- recipe program.py
+++ recipe program.py
@@ -1,3 +1,15 @@
 #recipe alteration program
 #olivia goodman 1/6/20
 #version 1 - this version will focus on creating the scale factor
+
+print("Pancake recipe courtesy of Chelsea Sugar (https://www.chelsea.co.nz/browse-recipes/sunday-pancake-recipe/)")
+ingredients = ["flour", "white sugar", "grated lemon zest", "milk", "egg", "butter"]
+units = ["cup", "tablespon", "teaspoon", "cup", "n/a", "grams"]
+amounts = [1, 2, 0.5, 1, 1, 30]
+old_servings = 3
+
+print("The current recipe serves {} people.".format(old_servings))
+new_servings = int(input("How many people should the recipe serve? "))
+
+scale_factor = new_servings / old_servings
+print(scale_factor)
