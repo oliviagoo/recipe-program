7a486e4b7c716ceb920e96aa60ff3cedbe1d9c20
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air-2.local>
2020-06-03 12:06:05 +1200

    Version Two
    In this version I focused on the next component of the code – altering the amounts of each ingredient in the recipe using the scale factor. I looped through the amounts variable and multiplied each value in the list by the scale factor. This new value is appended to a list of all the new amounts. Then the new recipe is outputted, with each ingredient and its relevant unit and amount on one line. At this point, n/a still shows up.

--- recipe program.py
+++ recipe program.py
@@ -1,15 +1,28 @@
 #recipe alteration program
-#olivia goodman 1/6/20
-#version 1 - this version will focus on creating the scale factor
+#olivia goodman 3/6/20
+#version 2 - this version will alter the recipe's amounts based on the scale factor
 
+#hard coded recipe
 print("Pancake recipe courtesy of Chelsea Sugar (https://www.chelsea.co.nz/browse-recipes/sunday-pancake-recipe/)")
 ingredients = ["flour", "white sugar", "grated lemon zest", "milk", "egg", "butter"]
-units = ["cup", "tablespon", "teaspoon", "cup", "n/a", "grams"]
+units = ["cup", "tablespon", "teaspoon", "cup", "n/a", "gram"]
 amounts = [1, 2, 0.5, 1, 1, 30]
+new_amounts = []
 old_servings = 3
 
+
+#input - servings amount
 print("The current recipe serves {} people.".format(old_servings))
 new_servings = int(input("How many people should the recipe serve? "))
 
+#calculating scale factor
 scale_factor = new_servings / old_servings
 print(scale_factor)
+
+#altering the amounts in the recipe
+for value in amounts:
+    new_amounts.append(value * scale_factor)
+
+#outputting new recipe
+for i in range(len(ingredients)):
+    print("{:.2f} {}s of {}".format(new_amounts[i], units[i], ingredients[i]))
