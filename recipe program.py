8bd313d3fffa1f32db5285698fc0a0301a506435
Olivia Goodman <oliviagoodman@Olivias-MacBook-Air-3.local>
2020-06-04 15:09:26 +1200

    Version Six
    In this version I added error handling for the boundaries. This was only relevant for the scale factor. So, if the new amount of servings that the user entered creates a scale factor that is greater than three or smaller than one quarter, the program stops them and prompts them to enter a different amount. I put the new servings input down with the scale factor calculation, and put both in a while True loop. This loops infinitely until the user enters a new serving amount that leads to a scale factor that is between 0.25 and 3.

--- recipe program.py
+++ recipe program.py
@@ -1,7 +1,8 @@
 #recipe alteration program
 #olivia goodman 3/6/20
-#version 5 - this version will replace the hard-coded amounts and old serving amount
+#version 6 - this version will focus on boundary error handling
 
+#this function gets the user input for the original recipe
 def recipe_input():
     while True:
         new_ingredient = input("Enter an ingredient, or press Q to quit: ").lower().strip()
@@ -16,28 +17,34 @@ def recipe_input():
 
 #main routine
 
-#hard coded recipe
 #print("Pancake recipe courtesy of Chelsea Sugar (https://www.chelsea.co.nz/browse-recipes/sunday-pancake-recipe/)")
+#setting up the necessary empty lists
 ingredients = []
 units = []
 amounts = []
 new_amounts = []
 old_servings = recipe_input()
 
-
-#input - servings amount
-print("The current recipe serves {} people.".format(old_servings))
-new_servings = int(input("How many people should the recipe serve? "))
-
-#calculating scale factor
-scale_factor = new_servings / old_servings
-print(scale_factor)
+#input - how many people the recipe should serve
+while True:
+    new_servings = int(input("How many people should the recipe serve? "))
+    #calculating scale factor
+    scale_factor = new_servings / old_servings
+    print(scale_factor)
+    if scale_factor > 3:
+        print("The ingredients won't fit in the bowl! Please make a smaller batch.")
+    elif scale_factor < 0.25:
+        print("Your recipe is too small! Make a larger batch and freeze the leftovers.")
+    else:
+        break
+    
 
 #altering the amounts in the recipe
 for value in amounts:
     new_amounts.append(value * scale_factor)
 
 #outputting new recipe
+print("This recipe now serves {} people.".format(new_servings))
 for i in range(len(ingredients)):
     print("{:.2f} {}s of {}".format(new_amounts[i], units[i], ingredients[i]))
 
